b86d5b8406e5ad606eb0a8e544b7d5bac27d2d63
eduardoBabilon <eduardobabilon19@gmail.com>
2025-06-17 14:05:04 -0300

    First Commit

---
+++ src/test/java/com/mills/rvt/RvtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mills.rvt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RvtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
